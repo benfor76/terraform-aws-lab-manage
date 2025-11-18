d7a33cb651bd8226f972455f5cf31b6e10f03ce3
Ben Forrester <ben_forrester@hotmail.com>
2025-11-18 12:55:42 -0500

    test1

--- main.tf
+++ main.tf
@@ -1,15 +1,20 @@
-# Data source to find the instances
-data "aws_ec2_instances" "rhel10_instances" {
-  instance_state_names = ["running", "stopped"] # Search in both running and stopped states
+provider "aws" {
+  region = "us-east-1"
+}
+
+# 1. Get all EC2 instances with Name tag containing "RHEL10"
+data "aws_instances" "rhel10" {
   filter {
     name   = "tag:Name"
-    values = ["*RHEL10*"] # Using a wildcard to match any name containing 'RHEL10'
+    values = ["*RHEL10*"]
   }
 }
 
-# Resource to ensure each found instance is started
-resource "aws_ec2_instance_state" "start_rhel10" {
-  for_each = toset(data.aws_ec2_instances.rhel10_instances.ids)
-  instance_id = each.value
-  state       = "running"
+# 2. Start the instances using SSM Automation
+resource "aws_ssm_automation_execution" "start_rhel10" {
+  document_name = "AWS-StartEC2Instance"
+
+  parameters = {
+    InstanceId = data.aws_instances.rhel10.ids
+  }
 }
