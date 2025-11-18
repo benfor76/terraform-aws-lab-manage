fc17408f2f0404b107a64adca14692602f6f97a0
Ben Forrester <ben_forrester@hotmail.com>
2025-11-18 12:48:04 -0500

    test1

--- main.tf
+++ main.tf
@@ -1,68 +1,17 @@
-provider "aws" {
-  region = var.aws_region
-}
-
-# Data source to find the AAPPG-RHEL10 instance
-data "aws_instances" "aappg_rhel10" {
-  filter {
-    name   = "tag:Name"
-    values = ["AAPPG-RHEL10"]
-  }
-
-  filter {
-    name   = "instance-state-name"
-    values = ["stopped"]
+# Get all instances with RHEL10 tag
+data "aws_instances" "all_rhel10" {
+  instance_tags = {
+    OS = "*-RHEL10"
   }
 }
 
-# Data source to find all other RHEL10 instances (excluding AAPPG-RHEL10)
-data "aws_instances" "other_rhel10" {
-  filter {
-    name   = "tag:Name"
-    values = ["*-RHEL10"]  # Matches any name ending with -RHEL10
-  }
-
-  filter {
-    name   = "instance-state-name"
-    values = ["stopped"]
-  }
-}
-
-# Local values to process instance IDs
-locals {
-  # Get AAPPG-RHEL10 instance ID (first one if multiple found)
-  aappg_instance_id = length(data.aws_instances.aappg_rhel10.ids) > 0 ? data.aws_instances.aappg_rhel10.ids[0] : null
-  
-  # Get other RHEL10 instances, excluding AAPPG-RHEL10
-  other_instance_ids = [
-    for id in data.aws_instances.other_rhel10.ids :
-    id if id != local.aappg_instance_id
-  ]
-}
-
-# Start AAPPG-RHEL10 instance first
-resource "aws_ec2_instance_state" "start_aappg" {
-  count = local.aappg_instance_id != null ? 1 : 0
-
-  instance_id = local.aappg_instance_id
-  state       = "running"
-}
-
-# Wait for AAPPG-RHEL10 to be running before starting others
-resource "time_sleep" "wait_for_aappg" {
-  count = local.aappg_instance_id != null ? 1 : 0
-
-  depends_on = [aws_ec2_instance_state.start_aappg]
-
-  create_duration = "60s"  # Wait 60 seconds for instance to fully start
-}
-
-# Start all other RHEL10 instances after AAPPG-RHEL10 is running
-resource "aws_ec2_instance_state" "start_other_instances" {
-  for_each = toset(local.other_instance_ids)
+# Start only stopped ones
+resource "aws_ec2_instance_state" "start_stopped_rhel10" {
+  for_each = toset([
+    for id in data.aws_instances.all_rhel10.ids : id
+    if data.aws_instances.all_rhel10.instances[index(data.aws_instances.all_rhel10.ids, id)].state == "stopped"
+  ])
 
   instance_id = each.value
   state       = "running"
-
-  depends_on = [time_sleep.wait_for_aappg]
 }
