7565c7886dc7c07b3f78fa3ed8192dcf80c6694d
Ben Forrester <ben_forrester@hotmail.com>
2025-11-18 12:52:39 -0500

    test1

--- main.tf
+++ main.tf
@@ -1,17 +1,15 @@
-# Get all instances with RHEL10 tag
-data "aws_instances" "all_rhel10" {
-  instance_tags = {
-    OS = "*-RHEL10"
+# Data source to find the instances
+data "aws_ec2_instances" "rhel10_instances" {
+  instance_state_names = ["running", "stopped"] # Search in both running and stopped states
+  filter {
+    name   = "tag:Name"
+    values = ["*RHEL10*"] # Using a wildcard to match any name containing 'RHEL10'
   }
 }
 
-# Start only stopped ones
-resource "aws_ec2_instance_state" "start_stopped_rhel10" {
-  for_each = toset([
-    for id in data.aws_instances.all_rhel10.ids : id
-    if data.aws_instances.all_rhel10.instances[index(data.aws_instances.all_rhel10.ids, id)].state == "stopped"
-  ])
-
+# Resource to ensure each found instance is started
+resource "aws_ec2_instance_state" "start_rhel10" {
+  for_each = toset(data.aws_ec2_instances.rhel10_instances.ids)
   instance_id = each.value
   state       = "running"
 }
